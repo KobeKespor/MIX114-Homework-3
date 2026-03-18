1b3a111f5502a2b04fee9f848338bdbd60cd84e0
Kasper Emil Larsen <Kasper100el@gmail.com>
2026-03-18 23:57:00 +0100

    la inn oppgave 1.b og oppgave 2 på nytt

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ function splitLines(text) {
     return text.split("\n").filter(line =>line !== "")
 }
 
+//Oppgave 1.b
+let textValue = ["1.2", "-3.4", "5.6"];
+
+function s2i (){
+  numbers = textValue.map(Number);
+  return numbers;
+}
+
+console.log(s2i())
+
 // Oppgave 1.c
 function sumPairs(numbers) {
     let list = document.getElementById("resultList");
@@ -14,3 +24,31 @@ function sumPairs(numbers) {
         list.innerHTML += item;
     }
 }
+
+// Oppgave 2
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+button.addEventListener("click", () => {
+  const task = input.value;
+
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `<input type="checkbox"/><span>${task}<span/><button>&#10005;<button/>`;
+
+  list.appendChild(listItem);
+
+  input.value = ""
+
+  const checkbox = listItem.querySelector("input");
+  const span = listItem.querySelector("span");
+  const cross = listItem.querySelector("button");
+
+  checkbox.addEventListener("change", () => {
+    span.style.textDecoration = checkbox.checked ? "line-through" : "none";
+  });
+
+  cross.addEventListener("click", () => {
+    listItem.remove();
+  });
+  });
